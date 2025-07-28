67389f2417f5478d92bca387d8120b53c0323e01
minhkhoait193 <dangminhkhoa190303@gmail.com>
2025-07-28 14:20:57 +0700

    Update visualize.py

--- visualize.py
+++ visualize.py
@@ -1,11 +1,14 @@
-import ast
+import ast # dùng để chuyển chuỗi thành list
 
-import cv2
+import cv2 #	Đọc / ghi video, vẽ hình, hiển thị văn bản
 import numpy as np
-import pandas as pd
+import pandas as pd #	Đọc file CSV, truy vấn dữ liệu theo frame_nmr
 
 
 def draw_border(img, top_left, bottom_right, color=(0, 255, 0), thickness=10, line_length_x=200, line_length_y=200):
+    
+    #Hàm này vẽ khung chữ nhật cho xe với 4 góc bo tròn (mỗi góc vẽ 2 đoạn ngắn).
+    
     x1, y1 = top_left
     x2, y2 = bottom_right
 
@@ -25,24 +28,27 @@ def draw_border(img, top_left, bottom_right, color=(0, 255, 0), thickness=10, li
 
 
 results = pd.read_csv('./test_interpolated.csv')
-
+ #Đọc dữ liệu từ file CSV đã được nội suy (kết quả sau add_missing_data.py)
 # load video
-video_path = 'sample.mp4'
+video_path = 'sample.mp4' #Mở video gốc và chuẩn bị ghi video mới
 cap = cv2.VideoCapture(video_path)
 
-fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # Specify the codec
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # Khai báo thông số video
 fps = cap.get(cv2.CAP_PROP_FPS)
 width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 out = cv2.VideoWriter('./out.mp4', fourcc, fps, (width, height))
 
+#ử lý biển số theo từng car_id
 license_plate = {}
 for car_id in np.unique(results['car_id']):
+    #Lấy license_number có độ chính xác cao nhất cho xe đó
     max_ = np.amax(results[results['car_id'] == car_id]['license_number_score'])
     license_plate[car_id] = {'license_crop': None,
                              'license_plate_number': results[(results['car_id'] == car_id) &
                                                              (results['license_number_score'] == max_)]['license_number'].iloc[0]}
-    cap.set(cv2.CAP_PROP_POS_FRAMES, results[(results['car_id'] == car_id) &
+    #Lấy frame tương ứng với biển số đó → crop hình
+    cap.set(cv2.CAP_PROP_POS_FRAMES, results[(results['car_id'] == car_id) & #frame_chứa_biển_số_đẹp_nhất
                                              (results['license_number_score'] == max_)]['frame_nmr'].iloc[0])
     ret, frame = cap.read()
 
@@ -51,6 +57,9 @@ for car_id in np.unique(results['car_id']):
 
     license_crop = frame[int(y1):int(y2), int(x1):int(x2), :]
     license_crop = cv2.resize(license_crop, (int((x2 - x1) * 400 / (y2 - y1)), 400))
+    #Lưu license_crop và license_number của xe vào dictionary license_plate
+
+    
 
     license_plate[car_id]['license_crop'] = license_crop
 
@@ -59,26 +68,26 @@ frame_nmr = -1
 
 cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
 
-# read frames
+# Lặp qua từng frame của video
 ret = True
 while ret:
     ret, frame = cap.read()
     frame_nmr += 1
     if ret:
-        df_ = results[results['frame_nmr'] == frame_nmr]
+        df_ = results[results['frame_nmr'] == frame_nmr] #Vẽ thông tin từng xe lên video Với từng dòng trong frame hiện tại
         for row_indx in range(len(df_)):
-            # draw car
+            # Vẽ khung xe (màu xanh lá)
             car_x1, car_y1, car_x2, car_y2 = ast.literal_eval(df_.iloc[row_indx]['car_bbox'].replace('[ ', '[').replace('   ', ' ').replace('  ', ' ').replace(' ', ','))
             draw_border(frame, (int(car_x1), int(car_y1)), (int(car_x2), int(car_y2)), (0, 255, 0), 25,
                         line_length_x=200, line_length_y=200)
 
-            # draw license plate
+            # Vẽ khung biển số (màu đỏ)
             x1, y1, x2, y2 = ast.literal_eval(df_.iloc[row_indx]['license_plate_bbox'].replace('[ ', '[').replace('   ', ' ').replace('  ', ' ').replace(' ', ','))
             cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 0, 255), 12)
 
-            # crop license plate
+            # Hiển thị ảnh biển số đã crop
             license_crop = license_plate[df_.iloc[row_indx]['car_id']]['license_crop']
-
+            #frame[vị_trí_phía_trên_đầu_xe] = license_crop
             H, W, _ = license_crop.shape
 
             try:
@@ -90,11 +99,12 @@ while ret:
 
                 (text_width, text_height), _ = cv2.getTextSize(
                     license_plate[df_.iloc[row_indx]['car_id']]['license_plate_number'],
+                    
                     cv2.FONT_HERSHEY_SIMPLEX,
                     4.3,
                     17)
 
-                cv2.putText(frame,
+                cv2.putText(frame, #Nếu không có lỗi → ghi frame ra video mới out.mp4
                             license_plate[df_.iloc[row_indx]['car_id']]['license_plate_number'],
                             (int((car_x2 + car_x1 - text_width) / 2), int(car_y1 - H - 250 + (text_height / 2))),
                             cv2.FONT_HERSHEY_SIMPLEX,
@@ -104,7 +114,13 @@ while ret:
 
             except:
                 pass
-
+    # A[Đọc test_interpolated.csv] --> B[Lặp từng car_id]
+    # B --> C[Chọn biển số rõ nhất]
+    # C --> D[Crop biển số và ghi nhớ]
+    # D --> E[Đọc từng frame video]
+    # E --> F[Truy vấn xe trong frame hiện tại]
+    # F --> G[Hiển thị khung xe + biển số + số xe]
+    # G --> H[Ghi vào out.mp4]
         out.write(frame)
         frame = cv2.resize(frame, (1280, 720))
 
